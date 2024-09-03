009d15e406bd41c06458cba0e0408169d464df29
Adam Weinberger <adamw@adamw.org>
2024-09-03 14:57:02 -0400

    filename_test: Simplify

--- context/filename_test.go
+++ context/filename_test.go
@@ -2,49 +2,42 @@ package context
 
 import "testing"
 
-var scenarios = []struct {
+var localScenarios = []struct {
 	expected string
 	ctx      *Ctx
 }{
 	{
 		expected: "output.txt",
 		ctx: &Ctx{
-			Filename: "output.txt",
+			Path: "output.txt",
 		},
 	},
 	{
 		expected: "output.txt",
 		ctx: &Ctx{
-			Filename: "/foo/bar/output.txt",
+			Path: "/foo/bar/output.txt",
 		},
 	},
 	{
 		expected: "output.txt",
 		ctx: &Ctx{
-			Filename: "./foo/output.txt",
+			Path: "./foo/output.txt",
+		},
+	},
+	{
+		expected: "foo",
+		ctx: &Ctx{
+			Path:     "output.txt",
+			Filename: "foo",
 		},
 	},
 }
 
-func assertFilename(t *testing.T, got *Ctx, expected string) {
-	if got.Destination != expected {
-		t.Errorf("expected %q, got %q", expected, got.Destination)
-	}
-}
 func TestSetLocalFilename(t *testing.T) {
-	c := &Ctx{Path: "output.txt"}
-	c.SetLocalFilename()
-	assertFilename(t, c, "output.txt")
-
-	c = &Ctx{Path: "/foo/bar/output.txt"}
-	c.SetLocalFilename()
-	assertFilename(t, c, "output.txt")
-
-	c = &Ctx{Path: "./foo/output.txt"}
-	c.SetLocalFilename()
-	assertFilename(t, c, "output.txt")
-
-	c = &Ctx{Path: "output.txt", Filename: "foo"}
-	c.SetLocalFilename()
-	assertFilename(t, c, "foo")
+	for _, s := range localScenarios {
+		s.ctx.SetLocalFilename()
+		if s.ctx.Destination != s.expected {
+			t.Errorf("expected %q, got %q", s.expected, s.ctx.Destination)
+		}
+	}
 }
